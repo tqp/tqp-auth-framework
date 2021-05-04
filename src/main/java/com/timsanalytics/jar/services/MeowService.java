52999ddcc89e44db942f5a80a831955541572ac6
tqp <timothyqpowers@gmail.com>
2021-05-04 01:32:56 -0400

    Progress towards releasing updates to Maven Central

--- src/main/java/com/timsanalytics/jar/services/MeowService.java
+++ src/main/java/com/timsanalytics/jar/services/MeowService.java
@@ -2,8 +2,14 @@ package com.timsanalytics.jar.services;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * A test class.
+ */
 @Service
 public class MeowService {
+    /**
+     * A test method.
+     */
     public void Meow() {
         System.out.println("The cat goes 'meow'.");
     }
